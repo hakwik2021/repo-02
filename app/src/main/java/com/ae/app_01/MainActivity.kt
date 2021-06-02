4727c25bf177e10ed0b5a3c564b8208c24f027cf
andy-iv <andy.edralin.iv@gmail.com>
2021-06-02 22:21:19 +0800

    first commit

---
+++ app/src/main/java/com/ae/app_01/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ae.app_01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * firebase project name :
+ * android project name  : app-01
+ * android project id    : com.ae.app_01
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
